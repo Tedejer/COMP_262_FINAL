e1839a4434469403e8d0e712baec73252e38e6de
Ternel <terneledejerwork@gmail.com>
2020-05-03 21:55:09 -0700

    First implementation

---
+++ src/crypto.h
@@ -0,0 +1,8 @@
+//
+// Created by Ternel on 5/3/2020.
+//
+
+#ifndef FINAL_CRYPTO_H
+#define FINAL_CRYPTO_H
+
+#endif //FINAL_CRYPTO_H
--- src/main.c
+++ src/main.c
@@ -1,43 +1,57 @@
-/**
- * C program to find all prime factors of a given number
- */
-
 #include <stdio.h>
+#include <time.h>
+#include <omp.h>
 
 int main()
 {
     int i, j, num, isPrime;
 
-    /* Input a number from user */
     printf("Enter any number to print Prime factors: ");
     scanf("%d", &num);
-
     printf("All Prime Factors of %d are: \n", num);
 
-    /* Find all Prime factors */
-    for(i=2; i<=num; i++)
+    clock_t begin = clock();
+    #pragma omp parallel
     {
-        /* Check 'i' for factor of num */
-        if(num%i==0)
+        // variables local to threads
+        int id;
+        double Pbegin,Pend;
+
+        id = omp_get_thread_num();
+        Pbegin = omp_get_wtime();
+
+        #pragma omp for
+        for (i = 2; i <= num; i++)
         {
-            /* Check 'i' for Prime */
-            isPrime = 1;
-            for(j=2; j<=i/2; j++)
+
+            if (num % i == 0)
             {
-                if(i%j==0)
+
+                isPrime = 1;
+                for (j = 2; j <= i / 2; j++)
                 {
-                    isPrime = 0;
-                    break;
+                    if (i % j == 0) {
+                        isPrime = 0;
+                        break;
+                    }
+                }
+
+                if (isPrime == 1)
+                {
+                    printf("%d ", i);
                 }
             }
-
-            /* If 'i' is Prime number and factor of num */
-            if(isPrime==1)
-            {
-                printf("%d, ", i);
-            }
         }
+
+        printf("\n");
+        Pend = omp_get_wtime();
+        printf("Thread(%d) finished in %f seconds \n", id, Pend - Pbegin);
     }
+
+    clock_t end = clock();
+    double time = (double)(end - begin);
+
+    printf("\nProgram executed in: %f seconds\n", time);
 
     return 0;
 }
