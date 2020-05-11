e3d459d75ab018646f82eabd9f4188bfe0e14c7b
Ternel Edejer <35849655+Tedejer@users.noreply.github.com>
2020-05-10 17:57:08 -0700

    Update crypto.c

--- src/crypto.c
+++ src/crypto.c
@@ -1,17 +1,25 @@
-
 #include "crypto.h"
 
+// to compile
+// do gcc -fopenmp crypto.c -lm
+// also you need to have the header file in the directory
+
 // A function to print all prime factors of a given number n 
-void primeFactors(int n) 
+int primeFactors(int n, int *factored) 
 { 
-    // Print the number of 2s that divide n 
-    #pragma omp parallel
+	int p = 0;
+	
+    // Print the number of 2s that divide n
+    #pragma omp parallel shared(n,p,factored) default(none)
     {
+		
         #pragma omp single
-        while (n%2 == 0) 
+        while (n %2 == 0)
         { 
             printf("%d ", 2); 
-            n = n/2; 
+            n = n/2;
+			factored[p] = n;
+			p++;
         } 
       
         // n must be odd at this point.  So we can skip  
@@ -24,6 +32,8 @@ void primeFactors(int n)
             { 
                 printf("%d ", i); 
                 n = n/i; 
+				factored[p] = n;
+				p++;
             } 
         }
 
@@ -32,8 +42,12 @@ void primeFactors(int n)
         #pragma omp single
         if (n > 2){ 
             printf ("%d ", n);
+			factored[p] = n;
+			p++;
         } 
     }
+	
+	return p;
 }
 
   
@@ -41,21 +55,40 @@ void primeFactors(int n)
 int main() 
 { 
 
-    int number; 
+    int number,p;
+	int factored[10];
 
     printf("Enter a number: ");
     scanf("%d,",&number);
-
-    clock_t begin = clock();
-
     printf("The prime factors for %d are:\n",number);
-    primeFactors(number); 
 
+	// CALCULATION
+    clock_t begin = clock();
+    p = primeFactors(number,factored);
     clock_t end = clock();
-    double time_spent = (double)(end - begin); 
 
+    double time_spent = (double)(end - begin);
+	
+	// STORE VALUES TO FILE
+    FILE *fpx;
+    fpx = fopen("Output.txt","w");
+    
+	for(int i = 0; i <= p; i++) 
+	{
+		
+		fprintf(fpx, "%d,", factored[i]);
+		fprintf(fpx,"\n");
+		
+		// PRINT OUT FACTORED ARRAY FOR DEBUG
+		printf("\n%d ",factored[i]);
+
+	}
+	
+    
+    fclose(fpx);
+	
     printf("\nThe execution time for this program took: %.1f seconds",time_spent);
-
+	printf("\n The size of p is %d",p);
     printf("\n");
     return 0; 
 }
